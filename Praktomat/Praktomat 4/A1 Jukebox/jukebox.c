3c22f8b45a0638bca13c5188441bfa79f7cc4feb
qutaiba alnimah <qutaiba.alnimah123@gmail.com>
2025-07-14 13:51:26 +0200

    jukebox

---
+++ Praktomat/Praktomat 4/A1 Jukebox/jukebox.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void Track_Search(FILE *file, const char *search)
+{
+    char line[200];
+    signed int lineNum = 1;
+    signed char found = 0;
+
+    while (fgets(line, sizeof(line), file))
+    {
+        if (strstr(line, search))
+        {
+            printf("Track %d: %s", lineNum, line);
+            found = 1;
+        }
+        lineNum++;
+    }
+
+    if (!found)
+    {
+        printf("Search word not found!");
+    }
+}
+
+int main(void)
+{
+    char fileName[50];
+    char search[100]; 
+
+    FILE *file;
+
+    printf("Please enter a Jukebox file name: ");
+    fgets(fileName, sizeof(fileName), stdin);
+    fileName[strcspn(fileName, "\n")] = 0; 
+
+    printf("Please enter search word: ");
+    fgets(search, sizeof(search), stdin);
+    search[strcspn(search, "\n")] = 0; 
+
+    file = fopen(fileName, "r");
+
+    if (file == NULL) 
+    {
+        printf("ERROR: File could not be found!\n");
+        return EXIT_FAILURE; 
+
+    Track_Search(file, search);
+    fclose(file);
+
+    return EXIT_SUCCESS;
+}
